75c62d7502e9e898bfd98fa69a2b5f1c3d700979
Shalabh Mishra <57224769+shalabhm24@users.noreply.github.com>
2019-12-01 17:46:28 +0530

    Functional Programs
    Small Programs to let my hands on experience on Fsharp

---
+++ Test/Test/File1.fs
@@ -0,0 +1,99 @@
+﻿module File2
+
+open System
+//Input from console
+let read _ = Console.ReadLine()
+let isValid = function null -> false | _ -> true
+let inputList = Seq.initInfinite read |> Seq.takeWhile isValid |> Seq.toList
+               // |>List.map(fun x->x|>int)
+
+//let reading parser =
+//    Seq.initInfinite (fun _ -> Console.ReadLine())
+//    |> Seq.choose (parser >> function true, v -> Some v | _ -> None)
+//Dividing head and tail part
+let numOfSeq = inputList.Head 
+let remList = inputList.Tail
+
+let remList2 = inputList.Tail.Head.Split(' ')|> Array.toList|>List.map(fun x->x |> int)|>List.take (numOfSeq|>int)
+                
+//-------------List Replication  ---------------------//
+let f n (arr:int list) = 
+        let mutable x = []
+        for j = 0 to arr.Length-1 do
+         for i=1 to n do
+           x <-arr.[j]::x 
+ 
+        x
+        |>List.rev
+
+//---------------Filter by given number---------------------//
+let filterfun n (arr:int list)=
+    let mutable x = []
+    let mutable y = []
+    for i=0 to arr.Length-1 do
+        match arr.[i]<n with
+        |true-> x <- arr.[i]::x
+        |false -> y <- arr.[i]::y
+
+    x|>List.rev
+
+//let result = filterfun numOfSeq remList
+//result|>List.iter(fun x-> printfn "%A" x)
+//printfn "%A" (filterfun numOfSeq remList)
+
+
+//------------revese a list---------------------//
+let revList (arr:int list) =
+    let mutable x = []
+    for i=0 to arr.Length-1 do
+        x<-arr.[i]::x
+    x
+
+//let res = revList inputList
+//res|>List.iter(fun x-> printfn "%A" x)
+
+//--------------Filter odd positions in a list-----------///
+
+let filterPos (arr:int list) =
+        let mutable x = []
+        let mutable y = []
+        for i=0 to arr.Length-1 do
+                match (i%2)=0 with
+                |true->  x<-arr.[i]::x
+                |false -> y<-arr.[i]::y
+
+        y
+
+//---------Sum of odd elements--------------------//
+let sumOdd (arr: int list)= 
+        arr|>List.filter(fun x->x%2<>0)
+        |>List.sum
+
+ //-----------E ^ x evaluation -------------//
+
+let power x y =
+  seq { 1..y } |> Seq.fold (fun m _ -> m * x) 1.0
+
+let factorial x =
+  seq { 1..x } |> Seq.fold (fun m v -> m * float(v)) 1.0
+
+let sum s =
+  s |> Seq.fold (fun s v -> s + v) 0.0
+
+let ex x =
+  seq { 0..9 }
+  |> Seq.map (fun n -> (power x n) / (factorial n))
+  |> sum
+
+
+//printfn "%A" (ex 2.0)
+
+
+
+//-------------------  Counting number of Socks Pair  -----------------------//
+
+let SockPair (arr:int list) = 
+    arr|>Seq.countBy(fun x->x)
+            |>Seq.filter(fun (a,b) -> b>1) 
+            |>Seq.map(fun (a,b) -> b/2)
+            |>Seq.toList|>List.sum 
---
+++ Test/Test/Program.fs
@@ -0,0 +1,39 @@
+﻿// Learn more about F# at http://fsharp.org
+    module File1
+    open FSharp.Core
+
+    let inline a x = x*x
+
+    let am = a 10
+    printf "%d" am
+    let inline printAsFloatingPoint number =
+        printfn "%f" (float number)
+
+    let printFloat (number) =
+        printfn "%f" (float number)
+
+    let c = printFloat 'c'
+
+    let f1 name = 
+        name + " FSharp"
+
+    let f2 name =
+        name + " Programming"
+
+    let composedFun = f1>>f2
+
+    printf "%A" (composedFun "Hello")
+
+    type Year =  
+       | January = 0  
+       | Fabruary = 1  
+       | March = 2  
+       | April = 3  
+  
+// Use of an enumeration.  
+    let monthName = enum<Year>(3)  
+    printf "%A" monthName  
+    let monthLiteral : Year = Year.January  
+    let n = int monthLiteral  
+    printf "\n%d" n 
+
